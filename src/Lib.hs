a872aa4291b8fde945c886832c384a534bbea591
Priyansh Bhardwaj <priyansh.bhardwaj@juspay.in>
2024-02-22 21:03:31 +0530

    Segregation Brackets and Operators

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,18 +9,24 @@ import Text.Read (readMaybe)
 import Control.Applicative ((<|>))
 import Data.Vector (Vector, (!?), empty, snoc)
 
-data Operator = ADDITION | SUBTRACTION | MULTIPLICATION | DIVISION | EXPONENT | OPENING_BRACKET | CLOSING_BRACKET
+data Symbol = OPERATOR Operator | BRACKET Bracket
   deriving (Eq, Show)
 
-instance Read Operator where
-  readsPrec :: Int -> ReadS Operator
-  readsPrec _ "+" = [(ADDITION, "")]
-  readsPrec _ "-" = [(SUBTRACTION, "")]
-  readsPrec _ "*" = [(MULTIPLICATION, "")]
-  readsPrec _ "/" = [(DIVISION, "")]
-  readsPrec _ "^" = [(EXPONENT, "")]
-  readsPrec _ "(" = [(OPENING_BRACKET, "")]
-  readsPrec _ ")" = [(CLOSING_BRACKET, "")]
+data Operator = ADDITION | SUBTRACTION | MULTIPLICATION | DIVISION | EXPONENT
+  deriving (Eq, Show)
+
+data Bracket = OPENING | CLOSING
+  deriving (Eq, Show)
+
+instance Read Symbol where
+  readsPrec :: Int -> ReadS Symbol
+  readsPrec _ "+" = [(OPERATOR ADDITION, "")]
+  readsPrec _ "-" = [(OPERATOR SUBTRACTION, "")]
+  readsPrec _ "*" = [(OPERATOR MULTIPLICATION, "")]
+  readsPrec _ "/" = [(OPERATOR DIVISION, "")]
+  readsPrec _ "^" = [(OPERATOR EXPONENT, "")]
+  readsPrec _ "(" = [(BRACKET OPENING, "")]
+  readsPrec _ ")" = [(BRACKET CLOSING, "")]
   readsPrec _  _  = []
 
 data Term = VARIABLE Int | CONSTANT Decimal
@@ -35,31 +41,45 @@ data Expression = TERM Term | COMPOSITE Operator Expression Expression
   deriving (Show)
 
 -- Lower to Higher: Left to Right
-precedenceOrder :: [[Operator]]
-precedenceOrder = [[ADDITION, SUBTRACTION], [MULTIPLICATION, DIVISION], [EXPONENT], [OPENING_BRACKET, CLOSING_BRACKET]]
+precedenceOrder :: [[Symbol]]
+precedenceOrder =
+  [ [OPERATOR ADDITION, OPERATOR SUBTRACTION]
+  , [OPERATOR MULTIPLICATION, OPERATOR DIVISION]
+  , [OPERATOR EXPONENT]
+  , [BRACKET OPENING , BRACKET CLOSING]
+  ]
 
-precedence :: Operator -> Int
+precedence :: Symbol -> Int
 precedence = precedenceAux 1 precedenceOrder
   where
-  precedenceAux :: Int -> [[Operator]] -> Operator -> Int
+  precedenceAux :: Int -> [[Symbol]] -> Symbol -> Int
   precedenceAux _ [] _ = 0
-  precedenceAux order (headOperators : otherOperators) operator
-    | operator `elem` headOperators = order
-    | otherwise = precedenceAux (order + 1) otherOperators operator
+  precedenceAux order (headSymbols : otherSymbols) operator
+    | operator `elem` headSymbols = order
+    | otherwise = precedenceAux (order + 1) otherSymbols operator
+
+isSymbol :: String -> Bool
+isSymbol = isJust . (readMaybe :: String -> Maybe Symbol)
 
 isOperator :: String -> Bool
-isOperator = isJust . (readMaybe :: String -> Maybe Operator)
+isOperator s = maybe False id $ readMaybe s >>= \ s -> case s of
+  OPERATOR _ -> Just True
+  _ -> Just False
+
+isBracket :: String -> Bool
+isBracket s = maybe False id $ readMaybe s >>= \ s -> case s of
+  BRACKET _ -> Just True
+  _ -> Just False
 
 isTerm :: String -> Bool
 isTerm = isJust . (readMaybe :: String -> Maybe Term)
 
-getOperator :: Operator -> Either String (Decimal -> Decimal -> Decimal)
-getOperator ADDITION = Right (+)
-getOperator SUBTRACTION = Right (-)
-getOperator MULTIPLICATION = Right (*)
-getOperator DIVISION = Right (/)
-getOperator EXPONENT = Right (\ a b -> a ^^ floor b)
-getOperator other = Left $ "Not supported for operation " <> show other
+getOperator :: Operator -> Decimal -> Decimal -> Decimal
+getOperator ADDITION = (+)
+getOperator SUBTRACTION = (-)
+getOperator MULTIPLICATION = (*)
+getOperator DIVISION = (/)
+getOperator EXPONENT = (\ a b -> a ^^ floor b)
 
 splitTokens :: String -> [String]
 splitTokens = dropWhile null . foldr (flip splitTokensAux) []
@@ -68,8 +88,8 @@ splitTokens = dropWhile null . foldr (flip splitTokensAux) []
   splitTokensAux [] c = splitTokensAux [[]] c
   splitTokensAux acc@([] : _) ' ' = acc
   splitTokensAux acc ' ' = [] : acc
-  splitTokensAux ([] : ar) c | isOperator [c] = [] : [c] : ar
-  splitTokensAux acc c | isOperator [c] = [] : [c] : acc
+  splitTokensAux ([] : ar) c | isSymbol [c] = [] : [c] : ar
+  splitTokensAux acc c | isSymbol [c] = [] : [c] : acc
   splitTokensAux (a : ar) c = (c : a) : ar
 
 parseExpression :: String -> Either String Expression
@@ -77,24 +97,21 @@ parseExpression string =
   let tokens = splitTokens string
   in parseExpressionAux tokens [] []
   where
-  parseExpressionAux :: [String] -> [Operator] -> [Expression] -> Either String Expression
-  parseExpressionAux [] (o : opr) (e1 : e2 : er) | not (isBracket o) = parseExpressionAux [] opr (COMPOSITE o e2 e1 : er)
+  parseExpressionAux :: [String] -> [Symbol] -> [Expression] -> Either String Expression
+  parseExpressionAux [] (OPERATOR o : opr) (e1 : e2 : er) = parseExpressionAux [] opr (COMPOSITE o e2 e1 : er)
   parseExpressionAux [] [] [expr] = Right expr
   parseExpressionAux (op : tr) (o : opr) exps
-    | isOperator op && read op == CLOSING_BRACKET && o == OPENING_BRACKET = parseExpressionAux tr opr exps
-  parseExpressionAux tokens@(op : _) (o : opr) (e1 : e2 : er)
-    | isOperator op && o /= OPENING_BRACKET && (read op == CLOSING_BRACKET || precedence (read op) <= precedence o)
+    | isSymbol op && read op == BRACKET CLOSING && o == BRACKET OPENING = parseExpressionAux tr opr exps
+  parseExpressionAux tokens@(op : _) (o'@(OPERATOR o) : opr) (e1 : e2 : er)
+    | isSymbol op && (read op == BRACKET CLOSING || precedence (read op) <= precedence o')
     = parseExpressionAux tokens opr (COMPOSITE o e2 e1 : er)
-  parseExpressionAux (op : tr) ops exps | isOperator op && notNextOperator tr = parseExpressionAux tr (read op : ops) exps
+  parseExpressionAux (op : tr) ops exps | isSymbol op && notNextOperator tr = parseExpressionAux tr (read op : ops) exps
   parseExpressionAux (tm : tr) ops exps | isTerm tm = parseExpressionAux tr ops (TERM (read tm) : exps)
-  parseExpressionAux s _ t = Left ("Invalid Expression: " <> string)
-
-  isBracket :: Operator -> Bool
-  isBracket = flip elem [OPENING_BRACKET, CLOSING_BRACKET]
+  parseExpressionAux _ _ _ = Left ("Invalid Expression: " <> string)
 
   notNextOperator :: [String] -> Bool
   notNextOperator [] = True
-  notNextOperator (x : _) = x == "(" || not (isOperator x)
+  notNextOperator (x : _) = not $ isOperator x
 
 printExpr :: Int -> Expression -> IO ()
 printExpr level ex = do
@@ -108,7 +125,7 @@ evaluateExprAux :: Vector Decimal -> Expression -> Either String Decimal
 evaluateExprAux _ (TERM (CONSTANT val)) = Right val
 evaluateExprAux array (TERM (VARIABLE idx)) = maybe (Left $ "Variable index out of range: " <> show idx) Right (array !? (idx - 1))
 evaluateExprAux array (COMPOSITE op e1 e2) = do
-  operation <- getOperator op
+  let operation = getOperator op
   res1 <- evaluateExprAux array e1
   res2 <- evaluateExprAux array e2
   if (op == EXPONENT && res2 - fromInteger (floor res2) > 0)
@@ -123,7 +140,7 @@ evaluateExpr array exprStr = do
 someFunc :: IO ()
 someFunc = do
   let exprStr = "(s5+s2)*(s3-s1)/((s4^s2+s3)*(s8/s2))"
-      vals = empty `snoc` 1 `snoc` 2.1 `snoc` 3 `snoc` 4 `snoc` 5 `snoc` 6 `snoc` 7 `snoc` 8
+      vals = empty `snoc` 1 `snoc` 2 `snoc` 3 `snoc` 4 `snoc` 5 `snoc` 6 `snoc` 7 `snoc` 8
       res = evaluateExpr vals exprStr
   print res
   return ()
